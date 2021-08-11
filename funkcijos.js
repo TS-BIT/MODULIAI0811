4ed2d8b0a0eb82431182f54168265d651e806620
TS BIT <tomassag@gmail.com>
2021-08-11 07:06:47 +0300

    Last changes 0810-0811

---
+++ funkcijos.js
@@ -0,0 +1,12 @@
+function suma(a, b) {
+    return a + b;
+}
+
+console.log(module);
+
+module.exports = {
+    suma
+};
+
+console.log(module);
+
---
+++ index.js
@@ -0,0 +1,8 @@
+const f = require("./funkcijos.js");
+
+//console.log(module);
+
+console.log("hello world");
+//console.log(suma(1, 2));
+
+console.log(f.suma(1, 2));
